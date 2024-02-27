08ce550b8c47fdd5d486335f87c4fad9f66f5226
Kristian <kristian.reale@gmail.com>
2024-02-27 12:58:11 +0100

    bug fix

--- pages/ASP To CNL.py
+++ pages/ASP To CNL.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests, json
 
-#URL = "http://localhost:5000"
-URL = "http://super-pancake-vw94vpvv7qhwww9-5000.app.github.dev"
+URL = "http://localhost:5000"
+#URL = "http://super-pancake-vw94vpvv7qhwww9-5000.app.github.dev"
 
 asp_input = None
 cnl_output = None
